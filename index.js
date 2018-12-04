8a12d7262116486f328f87b94e9a583c2d019522
Spatocode <impulseofficial069@gmail.com>
2018-12-04 17:04:04 +0100

    Made initial program arguments and command

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const program = require('commander')
+const fs = require('fs')
+const path = require('path')
+const { version } = require('./package.json')
+
 module.exports = () => {
-  console.log('Welcome to Hyron')
+  program
+    .name('hyron')
+    .version(`v${version}`, '--v, --version')
+    .option('init <option>', 'initialize a new hyron app')
+    .parse(process.argv)
+  if (program.init) console.log(`created a new hyron app`)
 }
