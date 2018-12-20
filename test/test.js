ffefef86593b64e47521cf88d4a1355ea94d1f69
Spatocode <impulseofficial069@gmail.com>
2018-12-20 20:13:12 +0100

    test: command with project name

--- test/test.js
+++ test/test.js
@@ -10,6 +10,42 @@ var TMPOBJ = tmp.dirSync().name
 var PKG = path.resolve(__dirname, '..', 'package.json')
 var BIN = path.resolve(path.dirname(PKG), require(PKG).bin.hyron)
 
+describe('Hyron', function () {
+  after(function (done) {
+    this.timeout(10000)
+    rimraf(TMPOBJ, done)
+  })
+
+  describe('<project-name>', function () {
+    var inst = env(this.fullTitle())
+
+    it('should create project directory', function (done) {
+      runCLI(inst.dir, ['foo'], function (err, code, stdout, stderr) {
+        if (err) return done(err)
+        inst.files = utils.parseFiles(stdout, inst.dir)
+        inst.stderr = stderr
+        inst.stdout = stdout
+        assert.strictEqual(inst.files.length, 11)
+        done()
+      })
+    })
+
+    it('should provide change directory instructions', function () {
+      assert.ok(/cd foo/.test(inst.stdout))
+    })
+
+    it('should provide install instructions', function () {
+      assert.ok(/npm install/.test(inst.stdout))
+    })
+
+    it('should have basic files', function () {
+      assert.notStrictEqual(inst.files.indexOf('foo/package.json'), 6)
+      assert.notStrictEqual(inst.files.indexOf('foo/app.js'), 7)
+      assert.notStrictEqual(inst.files.indexOf('foo/simpleApp.js'), 8)
+    })
+  })
+})
+
 function runCLI (dir, args, callback) {
   var argv = [BIN].concat(args)
   var binp = process.argv[0]
